b9cbb42f86eb03f94883ad3c3dc7e64f417264f5
Kaan <eniskaan98@gmail.com>
2022-09-27 04:31:58 +0300

    map

---
+++ Unity-StyChess/Assets/Scripts/tile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile : MonoBehaviour
+{
+    public GameObject unit;
+    [SerializeField]
+    public int x;
+    [SerializeField]
+    public int y;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if( x == null || y == null)
+        {
+            x = 0;
+            y = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
