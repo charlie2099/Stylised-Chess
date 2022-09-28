5dec7d73f87870d2c6153e0dab5ef9a96b426f66
Charlie <charlie3thomas.work@gmail.com>
2022-09-28 13:15:10 +0100

    [Sty-Chess] Added Knight model

--- Unity-StyChess/Assets/Scripts/gameboard.cs
+++ Unity-StyChess/Assets/Scripts/gameboard.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 public class gameboard : MonoBehaviour
 {
     public static gameboard Instance;
@@ -25,7 +27,7 @@ public class gameboard : MonoBehaviour
 
     void Start()
     {
-        CreateCoordinates();
+        //CreateCoordinates();
 
         foreach (var tile in gameboard_tiles)
         {
@@ -34,26 +36,49 @@ public class gameboard : MonoBehaviour
                 _piecesDict.Add(tile.piece, tile);
             }
         }
-        
+
         //Debug.Log("Tile: " + gameboard_tiles[7], gameboard_tiles[7]);
+
+
+        //Coordinates Button
+        coordinate_button.onClick.AddListener(CreateCoordinates);
     }
-    
+    public Button coordinate_button;
+    bool is_cordinates_open = false;
+    private List<GameObject> cordinate_prefabs = new List<GameObject>();
     public void CreateCoordinates()
     {
-        for (int i = 0; i < gameboard_tiles.Count; i++)
+
+        if (is_cordinates_open == false)
         {
-            //Debug.Log(gameboard_tiles.Count);
-            var x = gameboard_tiles[i].transform.position.x;
-            var y = gameboard_tiles[i].transform.position.y;
-            var z = gameboard_tiles[i].transform.position.z;
-            GameObject t = Instantiate(text_prefab, new Vector3(x - 3 , y + 5 , z), Quaternion.identity);
-            var TEXT = t.GetComponentInChildren<TMP_Text>();
-            //var TEXT = t.Find("txt");
-            if (TEXT != null)
-                TEXT.text = gameboard_tiles[i].x.ToString() + "," + gameboard_tiles[i].y.ToString();
-            //else
+            is_cordinates_open = true;
+            for (int i = 0; i < gameboard_tiles.Count; i++)
+            {
+                //Debug.Log(gameboard_tiles.Count);
+                var x = gameboard_tiles[i].transform.position.x;
+                var y = gameboard_tiles[i].transform.position.y;
+                var z = gameboard_tiles[i].transform.position.z;
+                GameObject t = Instantiate(text_prefab, new Vector3(x - 3, y + 5, z), Quaternion.identity);
+                cordinate_prefabs.Add(t);
+
+
+                var TEXT = t.GetComponentInChildren<TMP_Text>();
+                //var TEXT = t.Find("txt");
+                if (TEXT != null)
+                    TEXT.text = gameboard_tiles[i].x.ToString() + "," + gameboard_tiles[i].y.ToString();
+                //else
                 //Debug.Log("Text is null");
+            }
         }
+        else if (is_cordinates_open == true)
+        {
+            //cordinate_prefabs.Clear();
+
+            // need to be able to toggle coords off
+                        
+            is_cordinates_open = false;
+        }
+
     }
 
     public Piece selected_piece;
